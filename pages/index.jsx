2feaa857a27b0d61c4d442f190fdd9168ed47092
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-08 08:33:08 +0330

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../dummy-data';
+import Layout from '../components/layout/layout'
 
 const HomePage = () => {
   const featuredEvents = getFeaturedEvents();
   
   return (
     <div>
+      <Layout>
       <EventList items={featuredEvents}/>
+
+      </Layout>
     </div>
   )
 }
