d503c8bee7d82a1fd6bd4b13ba751f1ec774f9d5
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-10-08 16:02:12 +0330

    index & [eventId] optimized

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 
-import { getEventById, getAllEvents } from '../../helpers/api-utils';
+import { getEventById, getFeaturedEvents } from '../../helpers/api-utils';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
@@ -38,15 +38,16 @@ export async function getStaticProps(context) {
     props: {
       selectedEvent: event,
     },
+    revalidate: 30,
   };
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
 
   return {
     paths: paths,
-    fallback: false,
+    fallback: 'blocking',
   };
 }
